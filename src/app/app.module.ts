71dc7e18bfaa7e13fac15f9c105fba8ad5fa825c
משפחת חדד <haim.hddd@gmail.com>
2020-08-03 23:33:47 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import {environment} from '../environments/environment';
+
+// Instructions ---->
+// Replace configPlaceholder with your firebase credentials
+import { DropzoneDirective } from './dropzone.directive';
+import { UploaderComponent } from './uploader/uploader.component';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
+import { FileSizePipe } from './upload-task/file-size.pipe';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DropzoneDirective, UploaderComponent, UploadTaskComponent, FileSizePipe],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/uploader/uploader.component.ts
@@ -0,0 +1,45 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'uploader',
+  templateUrl: './uploader.component.html',
+  styleUrls: ['./uploader.component.scss']
+})
+export class UploaderComponent {
+
+  @ViewChild('fileInputID',{ static: false }) fileInput :ElementRef;
+
+  isHovering: boolean;
+
+  files: File[] = [];
+
+  toggleHover(event: boolean) {
+    this.isHovering = event;
+  }
+
+  onDrop(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i));
+    }
+  }
+  cnt:number = 0;
+  openExplorer($event){
+    this.cnt++;
+    console.log("out"+this.cnt);
+    if(this.cnt < 2){
+      this.fileInput.nativeElement.click();
+    }
+    if(this.cnt==2){
+      $event.stopPropagation();
+      /* this.fileInput.nativeElement.click(); */
+    }
+    else{
+      this.cnt=0;
+    }
+
+  }
+
+  deleteInputFile(){
+    this.fileInput.nativeElement.value = null;
+  }
+}
