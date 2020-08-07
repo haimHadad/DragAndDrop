1cc475b700097f5fbe5652ac2b6a47c5f2288a04
משפחת חדד <haim.hddd@gmail.com>
2020-08-07 15:37:57 +0300

    One file each time

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -10,6 +10,7 @@ export class UploaderComponent {
   @ViewChild('fileInputID',{ static: false }) fileInput :ElementRef;
   isFileInputClicked :boolean= true;
   isHovering: boolean;
+  errorAmount:boolean = false;
 
   files: File[] = [];
 
@@ -18,9 +19,19 @@ export class UploaderComponent {
   }
 
   onDrop(files: FileList) {
-    for (let i = 0; i < files.length; i++) {
-      this.files.push(files.item(i));
+    if(files.length > 1){
+      this.errorAmount = true;
+      return;
     }
+    if(this.files.length > 0){
+      this.errorAmount = true;
+      return;
+    }
+    this.errorAmount = false;
+    this.files.push(files.item(0));
+    /* for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i));
+    } */
   }
 
   openExplorer($event){
@@ -30,6 +41,8 @@ export class UploaderComponent {
   }
 
   deleteInputFile(){
+    this.errorAmount = false;
+    this.files.splice(0,1);
     this.fileInput.nativeElement.value = null;//
   }
 }
